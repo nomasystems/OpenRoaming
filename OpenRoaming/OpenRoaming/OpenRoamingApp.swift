0e76cd203414d53511b06dadecc1bd3666124792
Daniel Garcia Villaverde <danielgvi@ext.zara.com>
2024-11-06 14:42:02 +0100

    Added the project in xcode

---
+++ OpenRoaming/OpenRoaming/OpenRoamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenRoamingApp.swift
+//  OpenRoaming
+//
+//  Created by Daniel García Villaverde on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenRoamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
